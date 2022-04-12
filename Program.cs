160160299e63abbc790f5994c020f3ca98697094
Timofey <terekhovt@gmail.com>
2022-04-13 00:45:38 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hello_world;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DokanNet.Dokan.Mount(new Hello_World(), "M:\\");
+        }
+    }
+}
